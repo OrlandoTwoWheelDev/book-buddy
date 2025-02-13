595641563183b3eb259113c5cee45c0e0bd18c45
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-02-13 14:48:44 -0500

    add book details and reg/login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react"
 import RegisterAndLogin from "./Register";
+import Homepage from "./Homepage";
+import BookDetails from "./Bookdetails";
 
 
 
 const App = () => {
 
-  const [books, setBooks] = useState([]);
   
-
-  useEffect(() => {
-
-    const getBooks = async () => {
-      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`);
-      const bookData = await response.json();
-      console.log(`Here are the Books`, bookData.books);
-      
-      
-      setBooks(bookData.books);
-    };
-    getBooks();
-  }, []);
-
 
 
   return (
@@ -29,22 +16,15 @@ const App = () => {
       <h1>Book Buddy</h1>
 
       <Link to="/">HomePage</Link>
-      <Link to="/bookdetails">Book Details</Link>
       <Link to="/registerandlogin">Reg/Login</Link>
 
       <Routes>
-        <Route path="/" element={<h1>HomePage</h1>} />
-        <Route path="/bookdetails" element={<h1>Book Details</h1>} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/bookdetails/:bookId" element={<BookDetails />} />
         <Route path="/registerandlogin" element={<RegisterAndLogin />} />
       </Routes>
 
-      <ul>
-        {books.map((book, id) => (
-          <li key={id}>
-            <h3>{book.title}</h3>
-            </li>
-        ))}
-      </ul>
+      
 
     </>
   )
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,29 +1,88 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RegisterAndLogin = () => {
-  
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [LoginEmail, setLoginEmail] = useState("");
+  const [LoginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
 
-  return(
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const info = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/register', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          FirstName: `${setFirstName}`,
+          LastName: `${setLastName}`,
+          Email: `${setEmail}`,
+          Password: `${setPassword}`,
+        }),
+      });
+      if (!info.ok) {
+        const errorData = await info.json();
+        console.error(errorData);
+        throw new Error(
+          `HTTP error! status: ${info.status}`
+        )
+      }
+      const registeredInfo = await info.json();
+      console.log(registeredInfo);
+
+      navigate("/")
+    } catch (error) {
+      console.error("Error during registration:", error);
+
+      alert("Registration failed. Please try again.");
+    }
+  };
+
+  return (
     <>
-    <h1>Regristration</h1>
-    
-    <form>
-      <input placeholder="FirstName" />
-      <input placeholder="LastName" />
-      <input placeholder="Email" />
-      <input type="password" name="Password" placeholder="Password" />
+      <h1>Regristration</h1>
 
-      <button>Register Now</button>
+      <p>You'll need to register a new account <br />
+        in order to checkout,<br />
+        reserve, or return books!</p>
 
-    </form>
+      <form onSubmit={handleSubmit}>
+        <input value={FirstName}
+          onChange={((event) => setFirstName(event.target.value))}
+          placeholder="FirstName" />
+        <input value={LastName}
+          onChange={((event) => setLastName(event.target.value))}
+          placeholder="LastName" /><br />
+        <input type="email" value={Email}
+          onChange={((event) => setEmail(event.target.value))}
+          id="email" name="email" placeholder="Email" />
+        <input value={Password}
+          onChange={((event) => setPassword(event.target.value))}
+          type="Password" name="Password" placeholder="Password" />
+
+        <button>Register Now</button>
+      </form>
+
+      <h3>Already have an account? Log In Here!</h3>
+
+      <form onSubmit={handleSubmit}>
+        <input value={LoginEmail}
+          onChange={((event) => setLoginEmail(event.target.value))}
+          placeholder="LoginEmail" />
+        <input value={LoginPassword}
+          onChange={((event) => setLoginPassword(event.target.value))}
+          placeholder="LoginPassword" />
+
+        <button>Log In</button>
+      </form>
     </>
-   
-
   )
- 
 }
-
-
 
 export default RegisterAndLogin
